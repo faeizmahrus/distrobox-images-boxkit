7b5fc35f6e6fc13537357600afe719c2ab495154
Faeiz Mahrus <faeizmahrus@outlook.com>
2024-12-22 18:36:01 +0600

    Create basebox-fedora

---
+++ scripts/basebox-fedora.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Fix dnf stuff
+echo "max_parallel_downloads = 20" >> /etc/dnf/dnf.conf
+echo "minrate = 262144" >> /etc/dnf/dnf.conf
+echo "timeout = 5" >> /etc/dnf/dnf.conf
+
+# Install RPMFusion stuff
+dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y rpmfusion-free-release-tainted rpmfusion-nonfree-release-tainted
+dnf config-manager setopt fedora-cisco-openh264.enabled=1
+dnf install -y ffmpeg intel-media-driver libva-intel-driver \
+    mesa-va-drivers-freeworld mesa-vdpau-drivers-freeworld
+
+
